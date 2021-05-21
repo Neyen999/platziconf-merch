3963c629153d62e6891e2076fcaaf607cc9926d3
Neyen Ezequiel Marinelli <neyen_gt@hotmail.com>
2021-05-21 11:17:05 -0300

    Maquetación y estilos del Home

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { Home, CheckOut, Information, Payment, Success, NotFound } from "../containers"
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+  Home,
+  CheckOut,
+  Information,
+  Payment,
+  Success,
+  NotFound,
+} from '../containers';
+import Layout from '../components/Layout';
 
 const App = () => (
-    <BrowserRouter>
+  <BrowserRouter>
+    <Layout>
       <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/checkout" component={CheckOut}/>
-        <Route exact path="/checkout/information" component={Information}/>
-        <Route exact path="/checkout/payment" component={Payment}/>
-        <Route exact path="/checkout/success" component={Success}/>
-        <Route component={NotFound}/>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/checkout" component={CheckOut} />
+        <Route exact path="/checkout/information" component={Information} />
+        <Route exact path="/checkout/payment" component={Payment} />
+        <Route exact path="/checkout/success" component={Success} />
+        <Route component={NotFound} />
       </Switch>
-    </BrowserRouter>
+    </Layout>
+  </BrowserRouter>
 );
 
 export default App;
