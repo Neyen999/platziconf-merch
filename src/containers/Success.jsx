ccc8ec698b71df6da640950411145694b4158375
Neyen Ezequiel Marinelli <neyen_gt@hotmail.com>
2021-05-21 14:36:36 -0300

    Maquetación y estilos del flujo de pago (Payment y Success)

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,8 +1,17 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import '../styles/components/Success.css';
 
 const Success = () => {
-  return <h1>Success</h1>;
+  return (
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Gracias por tu compra</h2>
+        <p>Tu pedido llegara en 3 dias a tu dirección</p>
+        <div className="Success-map">Google Maps</div>
+      </div>
+    </div>
+  );
 };
 
 export default Success;
