fd076806239e1ce13d073c6faeb2a8eb7308b214
Neyen Ezequiel Marinelli <neyen_gt@hotmail.com>
2021-05-24 13:22:48 -0300

    Agregando Leaflet y React Lefleat ante la imposibilidad de aplicar Google Maps sin pagar

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,23 +1,29 @@
+/* eslint-disable arrow-body-style */
 import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Map = () => {
-  const mapStyles = {
-    height: '50vh',
-    width: '100%',
-  };
-
-  const defaultCenter = {
-    lat: 19.4267261,
-    lng: -99.1718796,
-  };
-
   return (
-    <LoadScript googleMapsApiKey="AIzaSyBDHlI_uw6UaspoCFsOD0_tEBI35S2mGXY">
-      <GoogleMap mapContainerStyle={mapStyles} zoom={9} center={defaultCenter}>
-        <Marker position={defaultCenter} />
-      </GoogleMap>
-    </LoadScript>
+    // <LoadScript googleMapsApiKey="AIzaSyBDHlI_uw6UaspoCFsOD0_tEBI35S2mGXY">
+    //   <GoogleMap mapContainerStyle={mapStyles} zoom={9} center={defaultCenter}>
+    //     <Marker position={defaultCenter} />
+    //   </GoogleMap>
+    // </LoadScript>
+    <MapContainer
+      center={[-34.82116810687362, -58.25956387093632]}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[-34.82116810687362, -58.25956387093632]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 };
 
